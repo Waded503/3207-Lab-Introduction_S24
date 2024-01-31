b544fa73d5527d78f3b1585a3b3c74f50a63168a
Waded503 <157040698+Waded503@users.noreply.github.com>
2024-01-31 08:45:22 -0500

    Update tuls.c

--- tuls.c
+++ tuls.c
@@ -1,47 +1,91 @@
 #include <dirent.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<string.h>
+#define MAX_LEN 10000
 
-int main(int argc, char *argv[]) {
-  // Get the directory to list.
-  char *dirname;
-  if (argc == 1) {
-    dirname = ".";
-  } else if (argc == 2) {
-    dirname = argv[1];
-  } else {
-    fprintf(stderr, "Usage: %s [dirname]\n", argv[0]);
-    exit(1);
-  }
 
-  // Open the directory.
-  DIR *dirp = opendir(dirname);
-  if (dirp == NULL) {
-    perror("opendir");
-    exit(1);
-  }
+int isDirectory(char* location) {
+    // opening the directory
+    DIR* directory = opendir(location);
+    // if directory is not null, then it is a valid directory
+    if (directory != NULL) {
+        // close the directory and return 1
+        closedir(directory);
+        return 1;
+    } else if (errno == ENOTDIR) {
+        // not a directory, so return 0
+        return 0;
+    }
+    // else return -1
+    return -1;
+}
 
-  // Scan the directory.
-  struct dirent **namelist;
-  int n = scandir(dirname, &namelist, NULL, alphasort);
-  if (n == -1) {
-    perror("scandir");
-    exit(1);
-  }
+void printDirectories(char* directoryName, int tabCount) {
+    int result = isDirectory(directoryName);    // checking the directory
+    if (result != -1) {
+        // the directory possibly exists
+        if (result == 1) {
+            // it is a directory
+            // scan the directory for the file contents
+            struct dirent **namelist;
+            int n = scandir(directoryName, &namelist, NULL, alphasort);
+            if (n == -1) {
+                // directory could not be opened, return with exit status of 1
+                perror("directory cannot be opened.");
+                exit(1);
+            }
+            while (n--) {
+                // printing the contents of the directory
+                // tabCount for visual hierarchy
+                for (int i = 0; i < tabCount; i += 1) {
+                    printf("\t");
+                }
+                // display the name
+                printf("%s\n", namelist[n]->d_name);
+                // getting the relative location of the file/directory in the current directory
+                char currentFileLocation[MAX_LEN] = "";
+                // current File location = "directoryName/currentName"
+                strcpy(currentFileLocation, directoryName);
+                strcat(currentFileLocation, "/");
+                strcat(currentFileLocation, namelist[n]->d_name);
+                if (strcmp(namelist[n]->d_name, ".") != 0 && strcmp(namelist[n]->d_name, "..") != 0) {
+                    // if it is a directory, recursively call printDirectories function
+                    // for the currentFileLocation, with increment in tabCount for visual hierarchy
+                    if (isDirectory(currentFileLocation) == 1)
+                        printDirectories(currentFileLocation, tabCount+1);
+                }
+                // free the pointer
+                free(namelist[n]);
+            }
+        } else {
+            // display error
+            perror("The given location is not a directory");
+            exit(1);
+}
+    } else {
+        // display error
+        perror("Directory doesn't exist.");
+        exit(1);
+    }
+}
 
-  // Print the directory contents.
-  for (int i = 0; i < n; i++) {
-    printf("%s\n", namelist[i]->d_name);
-  }
+int main(int argc, char** argv) {
 
-  // Free the memory allocated by scandir().
-  for (int i = 0; i < n; i++) {
-    free(namelist[i]);
-  }
-  free(namelist);
+    // getting the arguments
+    if (argc > 2) {
+        perror("Wrong number of arguments.\n");
+        exit(1);
+    } else {
+        // if argc = 1, then no directory location was sent, so set default location to "."
+        if (argc == 1) {
+            // print using default location
+            printDirectories(".", 0);
+        } else {
+            // print directories with the location provided
+            printDirectories(argv[1], 0);
+        }
+    }
 
-  // Close the directory.
-  closedir(dirp);
-
-  return 0;
 }
